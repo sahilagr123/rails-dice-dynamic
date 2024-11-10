47ae2cb61eaf8c610f7e3d62cc161d514dab736f
sahilagr123 <sahilagr@uchicago.edu>
2024-11-10 16:06:03 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get("/dice/2/6", { :controller => "zebra", :action => "giraffe"})
+  
+  get("/dice/2/10", { :controller => "zebra", :action => "elephant"})
 
+  get("/dice/1/20", { :controller => "zebra", :action => "lion"})
+
+  get("/dice/5/4"), { :controller => "zebra", :action => "rhino"}
+
+  get("/", { :controller => "zebra", :action => "rules"})
 end
